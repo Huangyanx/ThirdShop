4e01e7fa720884a5ce3866ef06aeaa0f60f9566d
qianweijian <315905217@qq.com>
2017-03-29 17:04:00 +0800

    修改左侧滚动条and 列表小细节 for 钱伟建

--- js/index.js
+++ js/index.js
@@ -1,12 +1,11 @@
  
-
-
- $(function(){
+ 
  	//监听左边的导航选项，锚链接 
  	$('#left_nav').scrollspy({ target: '#left_nav' });
  	//滚动条监听左侧显示/隐藏
- 	$(window).scroll(function(){
- 		if(this.scrollY > 900){
+ 	$(window).scroll(function(){ 
+ 		var scrollBotton=Number($("#footer").offset().top)-Number($(this).scrollTop())-700;
+ 		if($(this).scrollTop() > 750 && scrollBotton > 0){
  			$("#left_nav").slideDown();	
  		}else{
  			$("#left_nav").slideUp();	
@@ -17,9 +16,8 @@
  			$(".catebox .catebox_list").hide();
  			$(".catebox").click(function() {  
  				$(".catebox .catebox_list").slideToggle();
-	 		} );
+	 		});
  		} 
-	
- })
+ 
  		
  
